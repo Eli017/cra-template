742db221fd29b488f18b15a4a7df1e1011390c79
elisokeland <esokeland@developertown.com>
2020-07-19 16:17:25 -0400

    Added basic component files.

---
+++ src/Pages/Example.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExampleComponent from "../Components/ExampleComponent";
+
+const Example = () => {
+    return (
+        <section>
+
+        </section>
+    );
+};
+
+export default Example;
