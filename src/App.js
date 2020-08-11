00c5edeae84f641dadd4307b536776ef87b26af3
elisokeland <esokeland@developertown.com>
2020-08-11 17:50:36 -0400

    Set All linter rules with prettier!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Example from "./Pages/Example";
 
 const App = () => {
   return (
     <div className="App">
-        <Router>
-            <Switch>
-                <Route exact path={"/"} component={() => <Example />} />
-            </Switch>
-        </Router>
+      <Router>
+        <Switch>
+          <Route exact path={"/"} component={() => <Example />} />
+        </Switch>
+      </Router>
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { axe, toHaveNoViolations } from "jest-axe";
-import {render} from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 /*Jest-axe checks for a11y accessibility violations, which should be checked per page.
@@ -10,9 +10,7 @@ This is proper way to test for these violations, do not deviate from its asynchr
 expect.extend(toHaveNoViolations);
 
 test("renders without a11y violations", async () => {
-  const { container } = render(
-      <App />
-  );
+  const { container } = render(<App />);
   const results = await axe(container);
   expect(results).toHaveNoViolations();
 });
